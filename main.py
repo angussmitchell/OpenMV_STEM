8d1ae5272c7c5a8400c625dd3800c026c891bcd2
amit0315 <amit0315@uni.sydney.edu.au>
2018-11-12 11:07:08 +1100

    ww

---
+++ main.py
@@ -0,0 +1,43 @@
+from pyb import USB_VCP
+usb = USB_VCP()
+
+import sensor, image, time, math, lcd
+
+sensor.reset()
+sensor.set_pixformat(sensor.RGB565)
+sensor.set_framesize(sensor.QQVGA2) # we run out of memory if the resolution is much bigger...
+sensor.skip_frames(time = 2000)
+sensor.set_auto_gain(False)  # must turn this off to prevent image washout...
+sensor.set_auto_whitebal(False)  # must turn this off to prevent image washout...
+lcd.init() # Initialize the lcd screen.
+
+f_x = (2.8 / 3.984) * 160 # find_apriltags defaults to this if not set
+f_y = (2.8 / 2.952) * 120 # find_apriltags defaults to this if not set
+c_x = 160 * 0.5 # find_apriltags defaults to this if not set (the image.w * 0.5)
+c_y = 120 * 0.5 # find_apriltags defaults to this if not set (the image.h * 0.5)
+
+#Plant ID's
+Murnong_id = 0
+Murnong_msg = "Murnong"
+KG_id = 1
+KG_msg = "Kangaroo Grass"
+
+
+def degrees(radians):
+    return (180 * radians) / math.pi
+
+while(True):
+    img = sensor.snapshot()
+
+    for tag in img.find_apriltags(fx=f_x, fy=f_y, cx=c_x, cy=c_y): # defaults to TAG36H11
+        img.draw_rectangle(tag.rect(), color = (255, 0, 0))
+        img.draw_cross(tag.cx(), tag.cy(), color = (0, 255, 0))
+        #Check for all April Tag types
+        if (tag.id() == Murnong_id)
+        {
+                lcd.write(Murning_msg)
+        }
+
+    lcd.display(img) # Take a picture and display the image.
+
+
